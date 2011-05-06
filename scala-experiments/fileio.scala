14110791e3a52e730da9964d2626682e2d47c6be
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2011-05-06 10:29:54 +0300

    Stupid solution to Scala task in Rosetta Stone

--- scala-experiments/fileio.scala
+++ scala-experiments/fileio.scala
@@ -1,5 +1,6 @@
-val src = scala.io.Source.fromFile("/etc/passwd")
-val dest = new java.io.PrintWriter(new java.io.File("test.txt"))
+val src = scala.io.Source.fromFile("input.txt")
+val dest = new java.io.PrintWriter(new java.io.File("output.txt"))
+// Read from source line by line and for each line print to file
 src.getLines().foreach(dest.println(_))
 dest.close()
 src.close()
