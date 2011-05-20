c6445545844a4c1497cdc678047d745937aa8d6c
Andrii V. Mishkovskyi <amishkovskiy@cloudmade.com>
2011-05-20 20:18:46 +0300

    Don't know what, but i'm commiting it anyway

--- scala-experiments/chat.scala
+++ scala-experiments/chat.scala
@@ -4,4 +4,15 @@ package net.mishkovskyi.chat
 import java.nio._
 import scala.actors._
 import scala.actors.Actor._
-import scala.collection.mutable.{ListBuffer}
+import scala.collection.mutable.{ListBuffer, HashMap}
+
+
+class Chat {
+
+  def start() = {
+    while (true) {
+
+    }
+  }
+
+}
