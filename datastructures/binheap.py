13decf75c90c343fc9bc2f206ab86b819c7fe498
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2011-05-25 09:09:59 +0300

    Whatever

---
+++ datastructures/binheap.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+
+class BinomialHeap(object):
+
+
+    def __init__(self):
+        raise NotImplementedError
+
+
+    def merge(self, other):
+        raise NotImplementedError
+
+
+    def insert(self, item):
+        raise NotImplementedError
+
+
+    def find_minimum(self):
+        raise NotImplementedError
+
+
+    def del_minimum(self):
+        raise NotImplementedError
+
+
+
+class BinomialTree(object):
+
+
+    def __init__(self):
+        raise NotImplementedError
