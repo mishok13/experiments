73a39d273a7f3d8f16b7ca1562172d5a8ff9dd3b
Andrii V. Mishkovskyi <amishkovskiy@cloudmade.com>
2011-08-09 16:49:44 +0300

    I don't even know if I'm doing this right, I should learn me some Java before trying this. But who cares?

--- helloworld-android/src/net/mishkovskyi/musings/HelloWorldActivity.java
+++ helloworld-android/src/net/mishkovskyi/musings/HelloWorldActivity.java
@@ -2,14 +2,30 @@ package net.mishkovskyi.musings;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.content.SharedPreferences;
 
 public class HelloWorldActivity extends Activity
 {
+    public static final String PREFERENCES = "preferences-helloworld";
+
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
+    public void onCreate(Bundle state)
     {
-        super.onCreate(savedInstanceState);
+        super.onCreate(state);
+        SharedPreferences settings = getSharedPreferences(PREFERENCES, 0);
+        tasks =
+        for (task in tasks) {
+            TextView taskView = new TextView(this);
+            taskView.setText();
+            R.layout.main.addView(taskView);
+        }
         setContentView(R.layout.main);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
     }
 }
